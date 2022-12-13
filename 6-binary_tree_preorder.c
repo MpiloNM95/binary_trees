e5587038857c28bda7ee919d3a492eb4a52f14ff
MpiloNM95 <mpilonmafu95@gmail.com>
2022-12-13 07:17:48 -0800

    task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,14 +8,17 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+	if (!tree || !func)
 	{
 		return;
 	}
 	else
 	{
-		func(tree->);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		func(tree->n);
+
+		if (tree->left)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right)
+			binary_tree_preorder(tree->right, func);
 	}
 }
