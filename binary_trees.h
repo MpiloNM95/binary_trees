fab056ef7d56b3309e47dbe87f03da411166b59b
MpiloNM95 <mpilonmafu95@gmail.com>
2022-12-13 02:30:22 -0800

    added libraries

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,4 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
-/* Libraries */
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -24,6 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct ninary_tree_s heap_t;
 
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -45,6 +45,3 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *);
-
-#endif /* BINARY_TREES_H */
